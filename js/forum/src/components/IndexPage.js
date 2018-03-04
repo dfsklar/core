bb00974285e20005d20aa4eda8f29de762b71716
David Sklar <dfsklar@gmail.com>
2018-03-04 14:28:45 -0500

    Correction of session number display on the "Discussion header"; we must ASSUME the taghero init occurs before this.

--- js/forum/src/components/IndexPage.js
+++ js/forum/src/components/IndexPage.js
@@ -232,7 +232,7 @@ export default class IndexPage extends Page {
             </div>
             <div className="IndexPage-results-header IndexPage-results-child">
               <div className="session-name">
-                 Session {this.current_tag.data.attributes.position + 1}:&nbsp;
+                 Session {this.current_tag.sessionNumber}:&nbsp;
                  {this.current_tag.data.attributes.name}
                  <div className="literal-discussion">Discussion</div>
               </div>
